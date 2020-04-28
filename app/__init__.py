9802bb62f1b520f05d224ea66cec3bd440af208c
y <upload234@mail.ru>
2020-04-28 03:53:33 +0300

    FIX: use unix-style path

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, '..\\..\\stan_bot/quotes.db')
+        DATABASE=os.path.join(app.instance_path, '../../stan_bot/quotes.db')
     )
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
