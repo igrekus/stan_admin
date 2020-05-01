3d26bce52e7dfe1ee82b136a790f0ff59655aeb3
igrekus <upload234@mail.ru>
2020-05-01 03:49:05 +0300

    MISC: fiddle with table formatting

--- src/views/Quote.vue
+++ src/views/Quote.vue
@@ -1,21 +1,30 @@
 <template>
     <v-content>
-      <v-container fluid fill-height>
+      <v-container fluid class="pt-12">
         <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md8>
-            <v-card class="elevation-4">
-              <v-toolbar color="primary" dark flat>
-                <v-toolbar-title>Quotes</v-toolbar-title>
-              </v-toolbar>
-              <v-card-text>
-                <v-data-table
-                        :headers="headers"
-                        :items="desserts"
-                        :items-per-page="5"
-                        class="elevation-1"
-                ></v-data-table>
-              </v-card-text>
-            </v-card>
+          <v-flex xs12 sm8 md7 class="elevation-4">
+              <v-card>
+                  <v-card-title dense>
+                      Quotes
+                      <v-spacer></v-spacer>
+                      <v-text-field
+                              v-model="search"
+                              label="Search"
+                              single-line
+                              hide-details
+                              dense
+                      ></v-text-field>
+                  </v-card-title>
+                  <v-data-table
+                          :headers="headers"
+                          :items="quotes"
+                          :search="search"
+                          :items-per-page="-1"
+                          hide-default-footer
+                          dense
+                          class="pb-3"
+                  ></v-data-table>
+              </v-card>
           </v-flex>
         </v-layout>
       </v-container>
@@ -27,99 +36,33 @@
       name: "Quote",
       data() {
         return {
+          search: '',
           headers: [
             {
-              text: 'Dessert (100g serving)',
-              align: 'start',
+              text: 'id',
               sortable: false,
-              value: 'name',
+              value: 'id',
+              divider: true,
+              width: '5%'
             },
-            {text: 'Calories', value: 'calories'},
-            {text: 'Fat (g)', value: 'fat'},
-            {text: 'Carbs (g)', value: 'carbs'},
-            {text: 'Protein (g)', value: 'protein'},
-            {text: 'Iron (%)', value: 'iron'},
+            {text: 'message_id', value: 'message_id', sortable: false, width: '5%'},
+            {text: 'text', value: 'text', sortable: false},
           ],
-          desserts: [
+          quotes: [
             {
-              name: 'Frozen Yogurt',
-              calories: 159,
-              fat: 6.0,
-              carbs: 24,
-              protein: 4.0,
-              iron: '1%',
+              text: 'Frozen Yogurt',
+              message_id: 159,
+              id: 6.0,
             },
             {
-              name: 'Ice cream sandwich',
-              calories: 237,
-              fat: 9.0,
-              carbs: 37,
-              protein: 4.3,
-              iron: '1%',
+              text: 'Ice cream sandwich',
+              message_id: 237,
+              id: 9.0,
             },
             {
-              name: 'Eclair',
-              calories: 262,
-              fat: 16.0,
-              carbs: 23,
-              protein: 6.0,
-              iron: '7%',
-            },
-            {
-              name: 'Cupcake',
-              calories: 305,
-              fat: 3.7,
-              carbs: 67,
-              protein: 4.3,
-              iron: '8%',
-            },
-            {
-              name: 'Gingerbread',
-              calories: 356,
-              fat: 16.0,
-              carbs: 49,
-              protein: 3.9,
-              iron: '16%',
-            },
-            {
-              name: 'Jelly bean',
-              calories: 375,
-              fat: 0.0,
-              carbs: 94,
-              protein: 0.0,
-              iron: '0%',
-            },
-            {
-              name: 'Lollipop',
-              calories: 392,
-              fat: 0.2,
-              carbs: 98,
-              protein: 0,
-              iron: '2%',
-            },
-            {
-              name: 'Honeycomb',
-              calories: 408,
-              fat: 3.2,
-              carbs: 87,
-              protein: 6.5,
-              iron: '45%',
-            },
-            {
-              name: 'Donut',
-              calories: 452,
-              fat: 25.0,
-              carbs: 51,
-              protein: 4.9,
-              iron: '22%',
-            },
-            {
-              name: 'KitKat',
-              calories: 518,
-              fat: 26.0,
-              carbs: 65,
-              protein: 7,
-              iron: '6%',
+              text: 'Eclair',
+              message_id: 262,
+              id: 16.0,
             },
           ],
         }
