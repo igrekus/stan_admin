44ff41009a7268f85f6c809ff0ec8cd395adc6e6
igrekus <upload234@mail.ru>
2020-10-02 01:19:31 +0300

    stub server stuff

--- app/api_endpoints.py
+++ app/api_endpoints.py
@@ -1,4 +1,6 @@
-from flask import make_response, g
+import json
+
+from flask import make_response, g, request
 from flask_restful import Resource, reqparse
 from werkzeug.security import check_password_hash
 
@@ -12,6 +14,28 @@ parser.add_argument('password', type=str)
 class ApiPing(Resource):
     def get(self):
         return {'api': 'pong'}
+
+    def post(self):
+
+        data = json.loads(request.data)
+        print(data)
+
+        data['result'] = 'success'
+        resp = make_response(data)
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        # {'clientType': 'Юр. лицо', 'legalForm': 'АО', 'clientName': 'Техно-строй', 'personalTaxNum': '1234567890',
+        #  'ogrnNum': '0987654321', 'legalAddr': 'Москва, Сущёвский вал, 10', 'actualAddr': 'Москва, Сущёвский вал, 10',
+        #  'buildingPurpose': 'ФОК', 'buildingNote': 'стадион на 1000 мест', 'contactName': 'Иванов И.И.',
+        #  'contactPhone': '+7 (999) 123-45-67', 'contactJob': 'менеджер проекта',
+        #  'contactEmail': 'manager-ivan@fok-42.ru'}
+        return resp
+
+    def options(self):
+        resp = make_response()
+        resp.headers['Access-Control-Allow-Headers'] = '*'
+        resp.headers['Access-Control-Allow-Methods'] = 'POST,GET,OPTIONS'
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
 
 
 class Login(Resource):
